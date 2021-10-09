90b796e7ff584e06e320ead932598de5487ed732
Ritwick01 <ritwick20461@iiitd.ac.in>
2021-10-10 01:52:59 +0530

    Verbose for 2,3,4 and respective classes

--- Assignment1/Assignment1.java
+++ Assignment1/Assignment1.java
@@ -23,16 +23,67 @@ class Vaccine{
     }       
 }
 
+class Hospital{
+    private String name;
+    private int pincode;
+    private int uid;
+
+    public Hospital(String naam, int num, int id) {
+        name = naam;
+        pincode = num;
+        uid = id;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public int getpincode() {
+        return pincode;
+    }
+
+    public int uid() {
+        return uid;
+    }       
+}
+
+class Citizen{
+    private String name;
+    private int age;
+    private long uid;
+
+    public Citizen(String naam, int num, long id) {
+        name = naam;
+        age = num;
+        uid = id;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public int getage() {
+        return age;
+    }
+
+    public long uid() {
+        return uid;
+    }       
+}
 
 
 
 
 public class Assignment1 {
     ArrayList<Vaccine> vaclist = new ArrayList<Vaccine>();
+    ArrayList<Hospital> hoslist = new ArrayList<Hospital>();
+    ArrayList<Citizen> janhit = new ArrayList<Citizen>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Assignment1 as = new Assignment1();
         Vaccine vc;
+        Hospital ho;
+        Citizen ct;
         System.out.println("-------------------CoWin Portal------------------");
         System.out.println("-------------------------------------------------");
         System.out.println("1. Add Vaccine");
@@ -69,6 +120,8 @@ public class Assignment1 {
                 int num = sc.nextInt();
                 sc.nextLine();
                 int uid = ++counth;
+                ho = new Hospital(name, num, uid);
+                as.hoslist.add(ho);
                 System.out.println("Hospital Name: "+ name + " ,Pincode: " + num + " ,Unique ID: " + uid);
             }
             else if (n == 3) {
@@ -82,6 +135,10 @@ public class Assignment1 {
                 if (num <= 18) {
                     System.out.println("Only above 18 are allowed.");
                 }
+                else {
+                    ct = new Citizen(name, num, uid);
+                    as.janhit.add(ct);
+                }
             }
             else if (n == 4) {
                 System.out.print("Enter Hospital ID: ");
@@ -93,9 +150,13 @@ public class Assignment1 {
                     int day = sc.nextInt();
                     System.out.print("Enter quantity: ");
                     int quant = sc.nextInt();
-                    System.out.print("Select Vaccine: ");
+                    System.out.print("Select Vaccine: "); 
+                    for (int j = 0; j < as.vaclist.size(); ++j) {
+                        System.out.println(j + " " + as.vaclist.get(j).getname());
+                    }
                     int selection = sc.nextInt();
-                    System.out.println("Slot added by Hospital "+ uid + " for Day " + day + " ,Available Quantity: " + quant + " Vaccine ");
+                    System.out.println("Slot added by Hospital "+ uid + " for Day: " + day + " ,Available Quantity: " + quant + " of Vaccine " + 
+                                        as.vaclist.get(selection).getname());
                 }
             }
 
