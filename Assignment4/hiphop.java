1bd8e011dc729707164227d8b89562ed521ac829
Ritwick01 <ritwick20461@iiitd.ac.in>
2021-11-28 19:56:55 +0530

    Classes completetion almost

--- Assignment4/hiphop.java
+++ Assignment4/hiphop.java
@@ -1,9 +1,44 @@
 import java.util.*;
 
-class Softtoys {
+class TileMap {
+    private HashMap<Integer, Softtoys> tilemap = new HashMap<>();
+    private Player p1;
 
-    public void list() {
-        Softtoys teddy = new Softtoys();
+    public TileMap() {
+        tilemap.put(1, new Softtoys("Teddy"));
+        tilemap.put(2, new Softtoys("Donald Duck"));
+        tilemap.put(3, new Softtoys("Mickey Mouse"));
+        tilemap.put(4, new Softtoys("Goofy"));
+        tilemap.put(5, new Softtoys("Minnie Mouse"));
+        tilemap.put(6, new Softtoys("Chotta Bheem"));
+        tilemap.put(7, new Softtoys("Penguin"));
+        tilemap.put(8, new Softtoys("Elephant"));
+        tilemap.put(9, new Softtoys("Fox"));
+        tilemap.put(10, new Softtoys("Lion"));
+        tilemap.put(11, new Softtoys("Tiger"));
+        tilemap.put(12, new Softtoys("Turtle"));
+        tilemap.put(13, new Softtoys("Snorlax"));
+        tilemap.put(14, new Softtoys("Jigglypuff"));
+        tilemap.put(15, new Softtoys("Chimchar"));
+        tilemap.put(16, new Softtoys("Snivy"));
+        tilemap.put(17, new Softtoys("Squirtle"));
+        tilemap.put(18, new Softtoys("Pikachu"));
+        tilemap.put(19, new Softtoys("Gru"));
+        tilemap.put(20, new Softtoys("Minion"));
+    }
+
+
+}
+
+class Softtoys {
+    private String name;
+    
+    public Softtoys(String naam) {
+        name = naam;
+    }
+
+    public String getname() {
+        return name;
     }
 }
 
